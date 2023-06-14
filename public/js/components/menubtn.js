db5bc681491285ed5b398e92534cee4c4f1f77bc
Kyle Hiroshi Young <93508000+HirotheChamp@users.noreply.github.com>
2023-06-13 22:34:25 -0700

    website more responsive

---
+++ public/js/components/menubtn.js
@@ -0,0 +1,36 @@
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn_burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav_item');
+let mouseCursor = document.querySelector('.cursor');
+let navLinks = document.querySelectorAll('.menu-nav_item')
+let inputBox = document.querySelectorAll('.inputBox')
+let projectLinks = document.querySelectorAll('.projects_item')
+let showMenu = false;
+
+
+
+menuBtn.addEventListener('click', toggleMenu);
+
+
+
+
+
+function toggleMenu(){
+    if(!showMenu){
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        navItems.forEach(item => item.classList.add('open'));
+
+        showMenu = true;
+    } else {
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        navItems.forEach(item => item.classList.remove('open'));
+
+        showMenu = false;
+    }
+}
